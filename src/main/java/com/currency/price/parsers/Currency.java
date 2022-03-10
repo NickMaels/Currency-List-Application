ea2a79a38dfe83cbce5c98ead58567b6c16ec6ac
Nick_Maels <wiglegmaels@gmail.com>
2022-03-10 16:00:27 +0200

    Minor Fixes, Update Link For MAIB, Added new Parser For Victoriabank, New Save Functionality

--- src/main/java/com/currency/price/parsers/Currency.java
+++ src/main/java/com/currency/price/parsers/Currency.java
@@ -16,10 +16,10 @@ import java.util.stream.Collectors;
 
 @Getter
 @Setter
-@Builder
 @Entity
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Currency {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,14 +35,26 @@ public class Currency {
 
     private String bank;
 
-    public static Currency toCurrency(String string){
+    public static Currency toCurrency(String string) {
         List<String> list = Arrays.stream(string.split(" "))
                 .collect(Collectors.toList());
 
-        return Currency.builder()
-                .name(list.get(0))
-                .buy(Double.parseDouble(list.get(1)))
-                .sell(Double.parseDouble(list.get(2)))
-                .build();
+        Currency result;
+
+        try {
+            result = Currency.builder()
+                    .name(list.get(0))
+                    .buy(Double.parseDouble(list.get(1)))
+                    .sell(Double.parseDouble(list.get(2)))
+                    .build();
+        } catch (NumberFormatException nfe) {
+            result = Currency.builder()
+                    .name(list.get(0))
+                    .buy(0.0)
+                    .sell(0.0)
+                    .build();
+        }
+
+        return result;
     }
 }
